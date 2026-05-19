17c01229642fdad95cd27c1b03996fe1209fd9be
Anjel-cmd <abantighosh01@gmail.com>
2026-05-19 11:40:26 +0530

    Fixed the error

--- src/routes/analyze.tsx
+++ src/routes/analyze.tsx
@@ -122,7 +122,8 @@ function Analyze() {
 
     formData.append("file", file);
 
-    fetch("http://localhost:5000/predict", {
+    const apiUrl = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
+    fetch(`${apiUrl}/predict`, {
       method: "POST",
       body: formData,
     })
